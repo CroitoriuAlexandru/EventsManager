5d7101bc542454c0bd2eb444f12d5384e951fea2
Alex <croitoriu.alexandru.code@gmail.com>
2023-10-11 05:46:12 -0900

    added requirements

--- EventsManager/manage.py
+++ EventsManager/manage.py
@@ -4,16 +4,17 @@ import os
 import sys
 from EventsManager.settings import base
 
+
 def main():
     """Run administrative tasks."""
-    
     if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EventsManager.settings.local')
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EventsManager.settings.local")
+
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EventsManager.settings.production')
-    
-    
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EventsManager.settings.base')
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "EventsManager.settings.production"
+        )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EventsManager.settings.base")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -25,5 +26,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
